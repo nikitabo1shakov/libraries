601e21856995054fd611db4c8d26d3e7574776e3
nikitabolshakov <nikitas.bolshakov@gmail.com>
2021-10-15 21:55:39 +0300

    добавление репозитория

---
+++ app/src/main/java/com/nikitabolshakov/libraries/data/repository/GithubUsersRepo.kt
@@ -0,0 +1,16 @@
+package com.nikitabolshakov.libraries.data.repository
+
+import com.nikitabolshakov.libraries.data.model.GithubUser
+
+class GithubUsersRepo {
+
+    private val repository = listOf(
+        GithubUser("login1"),
+        GithubUser("login2"),
+        GithubUser("login3"),
+        GithubUser("login4"),
+        GithubUser("login5")
+    )
+
+    fun getUsers() = repository
+}
